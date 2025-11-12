3c6ea8d53657cc01ffd6380d536427da68b1f09a
Mitchel Vostrez <mitch@voz.dev>
2025-11-11 19:06:17 -0600

    fix bad path name

--- Formula/worktree.rb
+++ Formula/worktree.rb
@@ -1,22 +1,22 @@
 class Worktree < Formula
   desc "Modern CLI tool for managing git worktrees with stack visualization"
-  homepage "https://github.com/Zertsov/wt"
+  homepage "https://github.com/Zertsov/worktree"
   version "0.0.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Zertsov/wt/releases/download/v#{version}/worktree-#{version}-macos-arm64.tar.gz"
+      url "https://github.com/Zertsov/worktree/releases/download/v#{version}/worktree-#{version}-macos-arm64.tar.gz"
       sha256 "sha256:ff9b73ea5837e73a744c8df8bdf913b407df410b84660932b2d29c9b5040574c"
     else
-      url "https://github.com/Zertsov/wt/releases/download/v#{version}/worktree-#{version}-macos-x64.tar.gz"
+      url "https://github.com/Zertsov/worktree/releases/download/v#{version}/worktree-#{version}-macos-x64.tar.gz"
       sha256 "sha256:e94acdb6d1db7514414718acc7567595feacb866d7a97f0ed763a8386ffb5c78"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/Zertsov/wt/releases/download/v#{version}/worktree-#{version}-linux-x64.tar.gz"
+      url "https://github.com/Zertsov/worktree/releases/download/v#{version}/worktree-#{version}-linux-x64.tar.gz"
       sha256 "sha256:9544d44bbee176300c59a4acb8d838e3f3072c45165585ecd8e349261e8531ca"
     end
   end
