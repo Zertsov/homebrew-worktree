14a02ab1eb7fe6d94a2d00d598c2db1817663c3e
Mitchel Vostrez <mitch@voz.dev>
2025-11-11 19:13:51 -0600

    fix sha checksums

--- Formula/worktree.rb
+++ Formula/worktree.rb
@@ -7,17 +7,17 @@ class Worktree < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/Zertsov/worktree/releases/download/v#{version}/worktree-#{version}-macos-arm64.tar.gz"
-      sha256 "sha256:ff9b73ea5837e73a744c8df8bdf913b407df410b84660932b2d29c9b5040574c"
+      sha256 "231acfdd6a57f4156be93277a16e54639d611c16f2e2edc5e1d408ebc56f275c"
     else
       url "https://github.com/Zertsov/worktree/releases/download/v#{version}/worktree-#{version}-macos-x64.tar.gz"
-      sha256 "sha256:e94acdb6d1db7514414718acc7567595feacb866d7a97f0ed763a8386ffb5c78"
+      sha256 "b79bf2c4018bada4758cf395562582d1c4fe7b418b1886c0b8b5f2796e0268ee"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/Zertsov/worktree/releases/download/v#{version}/worktree-#{version}-linux-x64.tar.gz"
-      sha256 "sha256:9544d44bbee176300c59a4acb8d838e3f3072c45165585ecd8e349261e8531ca"
+      sha256 "edde8bc38cc13aa16ee162d3f02d15a62509806e5d7ed033fc809d9cd2f4b55d"
     end
   end
 
@@ -37,4 +37,3 @@ class Worktree < Formula
     assert_match "worktree", shell_output("#{bin}/worktree --help")
   end
 end
-
